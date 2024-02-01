0751fdee02f941ed4147bf4a2380c17fac870fea
Joset Manihuari Yaricahua <jssmy>
2024-02-01 15:39:31 -0500

    feature/hero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent : () => import('./pages/home/home.component'),
+    },
+    {
+        path: 'hero',
+        loadComponent : () => import('./pages/home/components/hero/hero.component'),
     }
 ];
